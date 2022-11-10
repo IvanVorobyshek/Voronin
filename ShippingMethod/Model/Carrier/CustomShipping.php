e53b7e6c3ed2173b3782dc8cf70c001514533605
IvanVorobyshek <ivanvorobyshek@gmail.com>
2022-11-10 15:02:22 +0200

    module 4 remade

--- ShippingMethod/Model/Carrier/CustomShipping.php
+++ ShippingMethod/Model/Carrier/CustomShipping.php
@@ -2,12 +2,13 @@
 
 namespace Voronin\ShippingMethod\Model\Carrier;
 
+use Magento\Checkout\Model\Session;
 use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Quote\Model\Quote;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Quote\Model\Quote\Address\RateRequest;
 use Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory;
 use Magento\Quote\Model\Quote\Address\RateResult\MethodFactory;
-use Magento\Quote\Model\QuoteRepository;
 use Magento\Shipping\Model\Carrier\AbstractCarrier;
 use Magento\Shipping\Model\Carrier\CarrierInterface;
 use Magento\Shipping\Model\Rate\ResultFactory;
@@ -16,7 +17,7 @@ use Psr\Log\LoggerInterface;
 /**
  * Custom shipping model
  */
-class Customshipping extends AbstractCarrier implements CarrierInterface
+class CustomShipping extends AbstractCarrier implements CarrierInterface
 {
     public const COUNTRYRATE = ['US' => 3, 'CA' => 7, 'UA' => 2];
 
@@ -36,9 +37,9 @@ class Customshipping extends AbstractCarrier implements CarrierInterface
     private $rateMethodFactory;
 
     /**
-     * @var \Magento\Quote\Model\QuoteRepository
+     * @var Session
      */
-    private $quoteRep;
+    private Session $session;
 
     /**
      * @param ScopeConfigInterface $scopeConfig
@@ -46,7 +47,7 @@ class Customshipping extends AbstractCarrier implements CarrierInterface
      * @param LoggerInterface $logger
      * @param ResultFactory $rateResultFactory
      * @param MethodFactory $rateMethodFactory
-     * @param QuoteRepository $quoteRep
+     * @param Session $session
      * @param array $data
      */
     public function __construct(
@@ -55,13 +56,13 @@ class Customshipping extends AbstractCarrier implements CarrierInterface
         \Psr\Log\LoggerInterface $logger,
         \Magento\Shipping\Model\Rate\ResultFactory $rateResultFactory,
         \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $rateMethodFactory,
-        \Magento\Quote\Model\QuoteRepository $quoteRep,
+        Session $session,
         array $data = []
     ) {
         parent::__construct($scopeConfig, $rateErrorFactory, $logger, $data);
         $this->rateResultFactory = $rateResultFactory;
         $this->rateMethodFactory = $rateMethodFactory;
-        $this->quoteRep = $quoteRep;
+        $this->session = $session;
     }
 
     /**
@@ -69,9 +70,35 @@ class Customshipping extends AbstractCarrier implements CarrierInterface
      *
      * @return array
      */
-    public function getAllowedMethods()
+    public function getAllowedMethods():array
     {
         return [$this->_code => $this->getConfigData('name')];
+    }
+
+    /**
+     * Calculates Shipping Cost
+     *
+     * @param string $cid
+     * @return float
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
+    public function shippingCostCalc(string $cid):float
+    {
+        // get quote
+        $quote = $this->session->getQuote();
+        // Base_cart_total
+        $totalPrice = $quote->getBaseSubtotal();
+        // Custom_shipping_fee
+        $shippingCost = (float)$this->getConfigData('shipping_cost');
+        // Country_rate
+        $countryRate = self::COUNTRYRATE[$cid] ?? 1;
+        // Number_of_total_products_in_card
+        $totalQty = $quote->getItemsQty();
+        // Number_of_uniq_products_in_card
+        $uniqueQty = $quote->getItemsCount();
+        // Calculating the shipping cost
+        return ($totalPrice * $shippingCost * $uniqueQty) / $totalQty * $countryRate;
     }
 
     /**
@@ -98,27 +125,8 @@ class Customshipping extends AbstractCarrier implements CarrierInterface
         $method->setMethod($this->_code);
         $method->setMethodTitle($this->getConfigData('name'));
 
-        // get the quote ID
-        $req = $request->getAllItems();
-        $quoteId = (int)$req[0]->getData('quote_id');
-        // get quote
-        $quote = $this->quoteRep->get($quoteId);
-        // Base_cart_total
-        $totalPrice = $quote->getBaseSubtotal();
-        // Custom_shipping_fee
-        $shippingCost = (float)$this->getConfigData('shipping_cost');
-        // Country_rate
-        $cid = $request->getDestCountryId();
-        $countryRate = self::COUNTRYRATE[$cid] ?? 1;
-        // Number_of_total_products_in_card
-        $totalQty = $quote->getItemsQty();
-        if ($totalQty <= 0) {
-            return false;
-        }
-        // Number_of_uniq_products_in_card
-        $uniqueQty = $quote->getItemsCount();
-        // Calculating the shipping cost
-        $shippingCost = ($totalPrice * $shippingCost * $uniqueQty) / $totalQty * $countryRate;
+        //get ShippingCost
+        $shippingCost = $this->shippingCostCalc($request->getDestCountryId());
 
         $method->setPrice($shippingCost);
         $method->setCost($shippingCost);
